ca950389e2dddc4218868e52a9bbfaba3a90e092
Abhishek chidrawar <abhishekchidrawar@gmail.com>
2022-10-04 12:31:51 +0530

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+
+        public void UpdateStatus(int id, string orderstatus, string? paymentstatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if(orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderstatus;
+                if(paymentstatus != null)
+                {
+                    orderFromDb.PaymentStatus= paymentstatus;
+                }
+            }
+        }
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentItentId)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            orderFromDb.PaymentDate = DateTime.Now;
+            orderFromDb.SessionId = sessionId;
+            orderFromDb.PaymentIntentId = paymentItentId;
+        }
+    }
+}
